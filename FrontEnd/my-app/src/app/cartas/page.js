dddba8976e4d8af247995edfbbd3b251c696acda
FacundoRodrigue <162990489+FacundoRodrigue@users.noreply.github.com>
2024-10-14 18:59:35 -0300

    Tranqui

--- FrontEnd/my-app/src/app/cartas/page.js
+++ FrontEnd/my-app/src/app/cartas/page.js
@@ -2,15 +2,24 @@
 
 import CardTable from "@/Estructuras/CardTable";
 import styles from "./page.module.css";
+import Texto from "@/Components/Texto";
+import Button from "@/Components/Button";
 
 export default function Cartas() {
 
     return (
         <main className = {styles.main}>
-            <div className = {styles.informacion}></div>
-            <CardTable
+            <div className = {styles.informacion}>
+                <Texto></Texto>
+                <Texto></Texto>
+                <div className={styles.InfoBotones}>
+                    <Button variant = "normal" text = "Mis cartas"></Button>
+                    <Button variant = "normal" text = "Cartas Futbolito"></Button>
+                </div>
+            </div>
+            {/* <CardTable
             
-            ></CardTable>
+            ></CardTable> */}
         </main>
     )
 }
