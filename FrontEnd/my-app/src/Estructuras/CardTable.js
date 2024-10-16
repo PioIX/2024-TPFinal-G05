66bcceb0a43152ceea0c49652f401af5a1cfbece
octavio <163418607+octaviopenaa@users.noreply.github.com>
2024-10-16 13:06:38 -0300

    props agregadas

--- FrontEnd/my-app/src/Estructuras/CardTable.js
+++ FrontEnd/my-app/src/Estructuras/CardTable.js
@@ -7,7 +7,15 @@ export default function CardTable(Cadena) {
         <div>
             {Cadena.map((jugador) => (
               <Card
-                
+              media = {jugador.Media}
+              posicion = {jugador.Posicion}
+              nacionalidad = {jugador.Nacionalidad}
+              imagenJugador = {jugador.Imagen}
+              escudo = {jugador.Equipo}
+              nombreJugador = {jugador.Nombre}
+              ataque = {jugador.Ataque}
+              control = {jugador.Control}
+              defensa = {jugador.Defensa}
               />
             ))}
         </div>
