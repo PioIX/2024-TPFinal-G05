0bce42d6370d9ae0c4c077e259011cb85a7027b9
octaviopenaa <openaiannello@pioix.edu.ar>
2024-10-15 22:30:56 -0300

    bocaa

--- FrontEnd/my-app/src/Estructuras/Card.js
+++ FrontEnd/my-app/src/Estructuras/Card.js
@@ -4,18 +4,30 @@ import styles from "./Card.module.css"
 export default function Card(src) {
     return (
         <div className={styles.carta}>
-            <div>
-                <img className={styles.img} 
-                src="/images/fondoPagInicial.png"
-                alt="imagen"
-                >
-                </img>
-            </div>
-            <div>
-                <h1 className={styles.titulo}>Título</h1>
-            </div>
-            <div>
-                <div></div>
+            <div className={styles.seccion1}>
+                <div>
+                    <img className={styles.img}
+                        src="/Jugadores/Bundesliga/BorussiaDortmund/Adeyemi.png"
+                        alt="imagen">
+                    </img>
+                </div>
+                <div className={styles.fondostats}>
+                    <div>
+                        <h1 className={styles.titulo}>Título</h1>
+                    </div>
+                    <div className={styles.seccion2}>
+                        <div className={styles.imgstatA}>
+                            <p>23</p>
+                        </div>
+                        <div className={styles.imgstatC}>
+                            <p>23</p>
+                        </div>
+                        <div className={styles.imgstatD}>
+                            <p>23</p>
+                        </div>
+                    </div>   
+                </div>
+                
             </div>
         </div>
     )
