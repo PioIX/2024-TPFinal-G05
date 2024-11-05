899a01d80010710f756a3499b3c33f2df6a3758e
FacundoRodrigue <162990489+FacundoRodrigue@users.noreply.github.com>
2024-11-04 23:31:54 -0300

    Otro componente

---
+++ FrontEnd/my-app/src/app/home/eleccionModo/futbolitosBattle/BattleComponents/MensajeFin.js
@@ -0,0 +1,40 @@
+"use client"
+
+import Texto from "@/Components/Texto";
+import styles from "./MensajeFin.module.css"
+
+export default function MensajeFin({ resultado }) {
+    const [mensajeGanaste, setMensajeGanaste] = useState(false);
+    const [mensajePerdiste, setMensajePerdiste] = useState(false);
+    const [mensajeEmpataste, setMensajeEmpataste] = useState(false);
+
+    if (resultado === "Ganaste") {
+        setMensajeGanaste(true)
+    } else if (resultado === "Perdiste") {
+        setMensajePerdiste(true)
+    } else if (resultado === "Empataste") {
+        setMensajeEmpataste(true)
+    }
+    return (
+        <div>
+            {mensajeGanaste && (
+                <>
+                    <h1 className={styles.title}>{resultado}</h1>
+                    <Texto text = "Bien ahi, te ganaste dos sobres para que completes la coleccion"></Texto>
+                </>
+            )}
+            {mensajePerdiste && (
+                <>
+                    <h1 className={styles.title}>{resultado}</h1>
+                    <Texto text = "Mala suerte esta vez, tenes mas chances de sumar jugadores la proxima, te regalamos un sobre de consuelo malo"></Texto>
+                </>
+            )}
+            {mensajeEmpataste && (
+                <>
+                    <h1 className={styles.title}>{resultado}</h1>
+                    <Texto ></Texto>
+                </>
+            )}
+        </div>
+    )
+}
